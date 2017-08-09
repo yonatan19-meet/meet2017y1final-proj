c4698b0d8a69803a0f83d883a0599ad16f8028f2
rani19-meet <rani19@meet.mit.edu>
2017-08-09 14:50:28 +0200

    Stopping here

--- yonatan.py
+++ yonatan.py
@@ -1,6 +1,6 @@
 import turtle
 turtle.penup()
-monkey = turtle.clone()
+girl = turtle.clone()
 UP_ARROW = "Up"
 LEFT_ARROW = "Left"
 DOWN_ARROW = "Down"
@@ -14,28 +14,28 @@ direction = UP
 def up():
     global direction
     direction=UP
-    move_monkey()
+    move_girl()
     print("you pressed the up key")
 
 direction = DOWN
 def down():
     global direction
     direction=DOWN
-    move_monkey()
+    move_girl()
     print("you pressed the down key")
 
 direction = LEFT
 def left():
     global direction
     direction=LEFT
-    move_monkey()
+    move_girl()
     print("you pressed the left key")
 
 direction = RIGHT
 def right():
     global direction
     direction=RIGHT
-    move_monkey()
+    move_girl()
     print("you pressed the right key")
 
 turtle.onkeypress(up, UP_ARROW)
@@ -45,21 +45,24 @@ turtle.onkeypress(right, RIGHT_ARROW)
 
 turtle.listen()
 
-def move_monkey():
-    my_pos = monkey.pos()
+def move_girl():
+    my_pos = girl.pos()
     x_pos = my_pos[0]
     y_pos = my_pos[1]
 
     if direction==UP:
-        monkey.goto(x_pos, y_pos + 10)
+        girl.goto(x_pos, y_pos + 10)
         print("Up")
     elif direction==LEFT:
-        monkey.goto(x_pos - 10, y_pos)
+        girl.goto(x_pos - 10, y_pos)
         print("Left")
     elif direction==RIGHT:
-        monkey.goto(x_pos + 10, y_pos)
+        girl.goto(x_pos + 10, y_pos)
         print("Right")
     elif direction==DOWN:
-        monkey.goto(x_pos, y_pos - 10)
+        girl.goto(x_pos, y_pos - 10)
         print("Down")
-        
+
+
+
+
